42d339d04f11df6c970877c86ac823f26bb2ac4c
mirsakiyev <mirsakiyev@yahoo.com>
2016-06-02 00:13:46 -0400

    Added Private instances(_strength, _speed, _name) and public property for _name

---
+++ COMP123-Assignment-1/Hero.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP123_Assignment_1
+{
+    /**
+     * This class defines a hero
+     *
+     * @class Hero
+     * @field _name {string}
+     */
+    public class Hero
+    {
+        //----------------------------------------------------------------------------------------------
+        //--------------------------PRIVATE INSTANCE VARIABLES------------------------------------------
+        //----------------------------------------------------------------------------------------------
+        
+
+        private int _strength;
+        private int _speed;
+        private string _name;
+
+
+        //----------------------------------------------------------------------------------------------
+        //-----------------------------PUBLIC PROPERTIES------------------------------------------------
+        //----------------------------------------------------------------------------------------------
+
+        /**
+         * This is a property for our private _name field
+         * @property {string} Name
+         */
+        public string Name
+        {
+            get { return this._name; }
+            set { this._name = value; }
+        }
+
+
+        //  /**
+        //   * <summary>
+        //   * This is a public property for our private _age field
+        //   * </summary>
+        //   * 
+        //   * @property {int} Age
+        //  */
+        //  public int Age
+        //  {
+        //      get { return this._age; }
+        //      set { this._age = value; }
+        //  }
+
+
+        //  //----------------------------------------------------------------------------------------------
+        //  //------------------------------------CONSTRUCTORS----------------------------------------------
+        //  //----------------------------------------------------------------------------------------------
+
+        //  /**
+        //   * <summary>
+        //   *This is default empty constructor for the Person class
+        //   * </summary>
+        //   * 
+        //   * 
+        //   * @constructor Person
+        //  */
+        //  public Hero()
+        //  {
+        //      // Initialize public properties
+        //      this.Name = "unknown name";
+        //      this._age = 0;
+
+        //  }
+
+
+
+        //  /**
+        //  * <summary>This is the constructor that takes name as a parameter and passes 
+        //  * it to the _name private instance variable
+        //  * </summary>
+        //  * 
+        //  * @constructor Person
+        //  * @param {string} name
+        // */
+        //  public Hero(string name)
+        //  {
+        //      this.Name = name;
+        //      this.Age = 0;
+        //  }
+
+
+        //  /**
+        // * <summary>This is the constructor that takes age as a parameter and passes 
+        // * it to the _age private instance variable
+        // * </summary>
+        // * 
+        // * @constructor Person
+        // * @param {int} age
+        // * @param {string} name
+        //*/
+        //  public Hero(int age)
+        //  {
+        //      this.Name = "unknown name";
+        //      this.Age = age;
+        //  }
+
+
+
+        //  /**
+        // * <summary>This is the constructor that takes both name and age as a parameters
+        // * It assigns the local name value to the Name property
+        // * It assigns the local age value to the Age property
+        // * it to the _age private instance variable
+        // * </summary>
+        // * 
+        // * @constructor Person
+        // * @param {int} age
+        // * @param {string} name
+        //*/
+        //  public Hero(string name, int age)
+        //  {
+        //      this.Name = name;
+        //      this.Age = age;
+        //  }
+
+
+        //  //----------------------------------------------------------------------------------------------
+        //  //-----------------------------------METHODS----------------------------------------------------
+        //  //----------------------------------------------------------------------------------------------
+
+        //  /**
+        //   * <summary>
+        //   * This method ouputs the _name value with "says hello!!1" to the console
+        //   * </summary>
+        //   * 
+        //   * @method SaysHello
+        //   * @returns {void}
+        //  */
+        //  public void SaysHello()
+        //  {
+        //      Console.WriteLine(this.Name + " " + " says hello!!1");
+        //  }
+
+
+        //  /**
+        //   * <summary>
+        //   * This method ouputs the _name and the _age values 
+        //   * in the following format: _name + "is" + _age + "years old".
+        //   * </summary>
+        //   * 
+        //   * @method ShowAge
+        //   * @returns {void}
+        //  */
+        //  public void ShowAge()
+        //  {
+        //      Console.WriteLine(this.Name + " is " + this.Age + " years old");
+        //  }
+
+    }
+}
---
+++ COMP123-Assignment-1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/**
+ * Assignment #1
+ * Author : Aslan Mirsakiyev
+ * Student ID : 300850326
+ * Date : 02.06.2016
+ * Submitted to : Tom Tsiliopoulos
+ * GitHub link : 
+ *
+ * Instructions:
+
+    1. The Hero Class must include the following Properties and Methods: (23 Marks: Functionality):
+            a. Private Instance Variables: strength (int), speed (int), health (int) and name(string) (3
+                    Marks: Functionality).
+
+            b. Public Properties: Name (string). The value of the public Name property will be stored in
+                    the private name Instance Variable (1 Marks: Functionality).
+    
+            c. The constructor Method should take one parameter, name (string) and pass its value to
+            the private name Instance Variable. It will also call the generateAbilities method (3
+            Marks: Functionality)
+ *
+ *
+*/
+
+namespace COMP123_Assignment_1
+{
+    /**
+     * This class is the driver class for our Program
+     * 
+     * @class Program
+     */
+    public class Program
+    {
+        /**
+         * The main method for our driver class Program
+         *
+         * @method Main
+         * @param {string[]} args
+         */
+        public static void Main(string[] args)
+        {
+            string separator = "-------------------------------------";
+            //----------------------------------------------------------------------------------------------
+            //----------------Create a new instance(rick) of the Person class-------------------------------
+            //----------------------------------------------------------------------------------------------
+            Hero rick = new Hero();
+            rick.Name = "Rick";
+            rick.Age = 40;
+            rick.SaysHello();
+            rick.ShowAge();           
+        }
+    }
+}
